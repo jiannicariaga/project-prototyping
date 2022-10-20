ea0ebe6f3e9a41fc49fdc2bce7870f6277d805dd
Jianni Cariaga <jiannicariaga@gmail.com>
2022-10-20 06:23:38 +0000

    Refactored HTML and JS to request Geolocation permission.

--- geolocation-api/main.js
+++ geolocation-api/main.js
@@ -1,22 +1,21 @@
-function getLocation() {
+const $button = document.querySelector('button');
+const $position = document.querySelector('p');
+
+$button.addEventListener('click', event => getPosition());
+
+function getPosition() {
   if (!navigator.geolocation) {
-    // eslint-disable-next-line no-console
-    console.log('Geolocation is not supported in your browser.');
+    $position.textContent = 'Geolocation is not supported in your browser.';
     return;
   }
-  // eslint-disable-next-line no-console
-  console.log('Retrieving location...');
   navigator.geolocation.getCurrentPosition(
     position => {
       const latitude = position.coords.latitude;
       const longitude = position.coords.longitude;
-      // eslint-disable-next-line no-console
-      console.log(`Latitude: ${latitude}\nLongitude: ${longitude}`);
+      $position.innerHTML = `Latitude: ${latitude}<br>Longitude: ${longitude}`;
     },
-    () => {
-      // eslint-disable-next-line no-console
-      console.log('Unable to retrieve location.');
-    });
+    error => {
+      $position.textContent = error.message;
+    }
+  );
 }
-
-getLocation();
