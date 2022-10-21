075fe9a9b72a5b9bfd9e5d301700371a7ab46f30
Jianni Cariaga <jiannicariaga@gmail.com>
2022-10-21 17:05:03 +0000

    Loaded default map location and loaded device's current location.

--- geolocation-and-google-maps/main.js
+++ geolocation-and-google-maps/main.js
@@ -1,7 +1,21 @@
 const $button = document.querySelector('button');
 const $position = document.querySelector('p');
+const defaultPosition = { lat: 34.052235, lng: -118.243683 };
 
+window.addEventListener('load', event => initMap(defaultPosition));
 $button.addEventListener('click', event => getPosition());
+
+function initMap(position) {
+  const map = new window.google.maps.Map(document.querySelector('.map'), {
+    center: position,
+    zoom: 13
+  });
+  // eslint-disable-next-line no-unused-vars
+  const marker = new window.google.maps.Marker({
+    position,
+    map
+  });
+}
 
 function getPosition() {
   if (!navigator.geolocation) {
@@ -13,24 +27,10 @@ function getPosition() {
       const latitude = position.coords.latitude;
       const longitude = position.coords.longitude;
       $position.innerHTML = `Latitude: ${latitude}<br>Longitude: ${longitude}`;
+      initMap({ lat: latitude, lng: longitude });
     },
     error => {
       $position.textContent = error.message;
     }
   );
-}
-
-window.addEventListener('load', event => initMap());
-
-function initMap() {
-  const position = { lat: 34.052235, lng: -118.243683 };
-  const map = new window.google.maps.Map(document.querySelector('.map'), {
-    center: position,
-    zoom: 13
-  });
-  // eslint-disable-next-line no-unused-vars
-  const marker = new window.google.maps.Marker({
-    position,
-    map
-  });
 }
